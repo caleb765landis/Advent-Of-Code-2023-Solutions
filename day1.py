0e58b1d897439c154a76b5ee19be98c17c12c447
caleb765landis <caleb765landis@gmail.com>
2023-12-01 16:19:33 -0700

    cleaned up files

--- day1.py
+++ day1.py
@@ -1,4 +1,27 @@
 """
+--- Day 1: Trebuchet?! ---
+Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
+
+You've been doing this long enough to know that to restore snow operations, you need to check all fifty stars by December 25th.
+
+Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+
+You try to ask why they can't just use a weather machine ("not powerful enough") and where they're even sending you ("the sky") and why your map looks mostly blank ("you sure ask a lot of questions") and hang on did you just say the sky ("of course, where do you think snow comes from") when you realize that the Elves are already loading you into a trebuchet ("please hold still, we need to strap you in").
+
+As they're making the final adjustments, they discover that their calibration document (your puzzle input) has been amended by a very young Elf who was apparently just excited to show off her art skills. Consequently, the Elves are having trouble reading the values on the document.
+
+The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+
+For example:
+
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
+
+Consider your entire calibration document. What is the sum of all of the calibration values?
+
 --- Part Two ---
 Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
 
@@ -16,10 +39,40 @@ In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Addi
 What is the sum of all of the calibration values?
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-ANSWER AT BOTTOM
+ANSWERS AT BOTTOM
 """
 
-from day_1_part_1 import getSum, CalibratiionValueException
+# Simple Custom Exception Class
+class CalibratiionValueException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+# get Calibration Value for part one of the problem
+def getCalVal(line=""):
+    if len(line) == 0:
+        raise CalibratiionValueException("ERROR: Line cannot be empty.")
+
+    calVal = ""
+
+    for char in line:
+        if char.isdigit():
+            calVal += char
+            break
+
+    # get last digit
+    for char in line[::-1]:
+        if char.isdigit():
+            calVal += char
+            break
+
+    if len(calVal) != 2:
+        raise CalibratiionValueException(
+            "ERROR: Calibration Value must be a 2 digit number.")
+
+    return int(calVal)
+
 
 nums = {
     "one": 1,
@@ -35,15 +88,15 @@ nums = {
 }
 
 # get Calibration Value for part two of the problem
-def getCalVal(line=""):
+def getCalValStrs(line=""):
     if len(line) == 0:
         raise CalibratiionValueException("ERROR: Line cannot be empty.")
-    
+
     # find indexes for first and last appearances of an integer
 
     firstIntIndex = -1
     lastIntIndex = -1
-    
+
     for i, char in enumerate(line):
         if char.isdigit():
             firstIntIndex = i
@@ -76,7 +129,7 @@ def getCalVal(line=""):
                 first = str(num)
                 firstIntIndex = firstStrIndex
 
-            # otherwise first number that appears has already been found, 
+            # otherwise first number that appears has already been found,
             # so compare that first number's index with this index to see if this appears first
             # if this number's index is less than the current first appearing index, then this number appears first
             elif firstIntIndex > firstStrIndex:
@@ -97,7 +150,6 @@ def getCalVal(line=""):
             elif lastIntIndex < lastStrIndex:
                 last = str(num)
                 lastIntIndex = lastStrIndex
-        
 
     calVal = ""
     calVal += first
@@ -106,19 +158,36 @@ def getCalVal(line=""):
     if len(calVal) != 2:
         raise CalibratiionValueException(
             "ERROR: Calibration Value must be a 2 digit number.")
-    
-    return int(calVal)
 
+    return int(calVal)
 
 def main():
     # read input file
     with open("day_1_input.dat") as inf:
-        sum = getSum(inf, getCalVal)
+        pt1Sum = 0
+        pt2Sum = 0
 
-    print(f"Sum: {sum}")
+        for line in inf:
+            line.rstrip()
+            # handle errors if line doesn't have a two digit number
+            try:
+                # get calibration values
+                pt1CalVal = getCalVal(line)
+                pt2CalVal = getCalValStrs(line)
+
+                # add calibration values to sum
+                pt1Sum += pt1CalVal
+                pt2Sum += pt2CalVal
+            except CalibratiionValueException as e:
+                print(e)
 
 
-if __name__ == "__main__":
+    print(f"Part 1 Sum: {pt1Sum}")
+    print(f"Part 2 Sum: {pt2Sum}")
+
+
+if __name__ == '__main__':
     main()
 
-# ANSWER: 54985
+# PART 1 ANSWER: 55130
+# PART 2 ANSWER: 54985
